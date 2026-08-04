b1d3e2fe0492363ffa87d2bea26087c159009e42
Aditya Tiwari <aditya96937@gmail.com>
2026-08-04 14:00:08 +0530

    Time: 4 ms (91.37%), Space: 47.1 MB (65.48%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,33 +1,41 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        // adj list bnayenge
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < numCourses; i++) {
+
+        for(int i = 0; i < numCourses; i++){
             adj.add(new ArrayList<>());
         }
-        int[] indegree = new int[numCourses];
+
         for (int[] edge : prerequisites) {
             adj.get(edge[1]).add(edge[0]);
-            indegree[edge[0]]++;
-        }
-        Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) {
-                q.offer(i);
-            }
         }
 
-        int count = 0;
-        while (!q.isEmpty()) {
-            int node = q.poll();
-            count++;
-            for (int neigh : adj.get(node)) {
-                indegree[neigh]--;
-                if (indegree[neigh] == 0) {
-                    q.offer(neigh);
+        // then we create the vis array
+        int[] vis = new int[numCourses];
+        for(int i = 0; i < numCourses; i++){
+            if(vis[i] == 0){
+                if(dfs(i, vis, adj)){
+                    return false;
                 }
             }
         }
+        return true;
+    }
+    private boolean dfs(int node, int[] vis, ArrayList<ArrayList<Integer>> adj){
+        vis[node] = 1;
+        for(int it : adj.get(node)){
+            if(vis[it] == 0){
+                if(dfs(it, vis, adj)){
+                    return true;
+                }
+            }
+            else if(vis[it] == 1){
+                return true;
+            }
+        }
 
-        return count == numCourses;
+    vis[node] = 2;
+    return false;  
     }
 }
