595c6d0b79747d9a4797db520af71cb490ee7db3
Aditya Tiwari <aditya96937@gmail.com>
2026-09-01 22:38:22 +0530

    Time: 75 ms (45.38%), Space: 46.8 MB (69.11%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -1,29 +1,5 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
-        int n = s.length();
-        for (int len = 1; len <= n / 2; len++) {
-            if (n % len != 0) continue;
-            // to check krte hai agr pattern repeat hoga
-             boolean repeat = true;
-            for (int i = 0; i < n; i++) {
-                if (s.charAt(i) != s.charAt(i % len)) {
-                    repeat = false;
-                    break;
-                }
-            }
-
-            if (repeat) return true;
-            for (int i = 0; i < n; i++) {
-                if (s.charAt(i) != s.charAt(i % len)) {
-                    repeat = false;
-                    break;
-                }
-            }
-
-            if (repeat) return true;
-
-        }
-
-        return false;
+        return (s+s).substring(1, 2*s.length()-1).contains(s);
     }
 }
